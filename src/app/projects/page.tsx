1d36db5922380374c5c6e61131cb09ecb703c5a9
Vikas <101702902+NotYourEren@users.noreply.github.com>
2024-12-19 19:56:28 +0530

    refactor: update project details and enhance project card component

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,4 +1,82 @@
+import ProjectCard from '@/components/Project/ProjectCard'
+import CdBack from '@/components/shared/CdBack'
+import { topProjects } from '@/lib/constants'
+import { ArrowRight } from 'lucide-react'
+import Link from 'next/link'
+
 const page = () => {
-  return <div>Projects</div>
+  return (
+    <section className="pb-16 pt-8 md:mt-8 lg:mt-10">
+      <div className="flex flex-col items-center justify-center">
+        <h1 className="text-center font-neu text-5xl font-semibold tracking-tight">
+          Projects
+        </h1>
+        <h2 className="mt-3 text-center font-neu text-gray-400">
+          Projects I&apos;ve created.
+        </h2>
+        <svg
+          width="400"
+          height="80"
+          viewBox="0 0 200 40"
+          className="-translate-y-12 translate-x-[-86px]"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <defs>
+            <linearGradient
+              id="line-gradient"
+              x1="0%"
+              y1="0%"
+              x2="100%"
+              y2="0%"
+            >
+              <stop offset="0%" stopColor="#9400D3" stopOpacity="0" />
+              <stop offset="100%" stopColor="#4B0082" stopOpacity="0.8" />
+            </linearGradient>
+          </defs>
+          <path
+            d="M10 20 L 80 20 L 100 30 H 196"
+            stroke="url(#line-gradient)"
+            strokeWidth="1"
+            fill="none"
+            strokeLinecap="round"
+          />
+        </svg>
+      </div>
+      <div>
+        <div className="space-y-14">
+          {topProjects.map((project, index) => (
+            <ProjectCard
+              key={index}
+              delay={index * 0.2}
+              imageSrc={project.imageSrc}
+              videoSrc={project.videoSrc}
+              title={project.title}
+              tags={project.tags}
+              timeline={project.timeline}
+              tagline={project.tagline}
+              liveDemoHref={project.liveDemoHref}
+              sourceCodeHref={project.sourceCodeHref}
+            />
+          ))}
+        </div>
+      </div>{' '}
+      <div>
+        <h1 className="my-14 text-xl">
+          View more on{' '}
+          <Link
+            href="https://github.com/krey-yon?tab=repositories"
+            className="group inline-flex items-center justify-start bg-gradient-to-r from-green-200 via-green-400 to-purple-700 bg-clip-text font-neu text-transparent"
+          >
+            Github
+            <ArrowRight className="ml-2 inline-block size-4 rounded-full border border-transparent stroke-black stroke-1 transition-all duration-300 ease-linear group-hover:-rotate-45 group-hover:border-slate-950 group-hover:stroke-[2px] dark:stroke-white dark:group-hover:border-slate-50" />
+          </Link>
+        </h1>
+        <h1 className="mt-24 bg-gradient-to-r from-gray-200 via-gray-400 to-gray-600 bg-clip-text text-center font-neu text-4xl font-extrabold text-transparent opacity-30 md:text-7xl">
+          More Soon
+        </h1>
+      </div>
+      <CdBack />
+    </section>
+  )
 }
 export default page
--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -8,46 +8,13 @@ export const navItems: NavItems = [{ name: 'Projects', href: '/projects' }]
 export const topProjects = [
   {
     videoSrc:
-      'https://res.cloudinary.com/dpj8y0x4s/video/upload/v1729062244/20241016-0702-36.7349933_se5srd.mp4',
-    imageSrc: '/images/project/Pages.png',
-    title: 'Pages',
-    tags: ['Nextjs', 'Typescript', 'Shadcn UI', 'Accertnity UI', 'TailwindCSS'],
-    timeline: 'Aug 2024',
-    tagline: 'A website to read your favourite Mangas',
-    sourceCodeHref: 'https://github.com/krey-yon/Pages',
-    liveDemoHref: 'https://pages-nine-eta.vercel.app/',
-  },
-  {
-    imageSrc: '/images/project/Codehub.png',
-    videoSrc:
-      'https://res.cloudinary.com/dpj8y0x4s/video/upload/v1729062837/20241016-0712-39.9608416_lgupvx.mp4',
-    title: 'CodeHub',
-    tags: ['Reactjs', 'Nodejs', 'MongoDB', 'TailwindCSS', 'Passport.js'],
-    timeline: 'Jun 2024 - July 2024',
-    tagline: 'An interface to interact with Githubs API',
-    sourceCodeHref: 'https://github.com/krey-yon/Codehub',
-    liveDemoHref: 'https://codehub-d7oo.onrender.com/',
-  },
-  {
-    videoSrc:
-      'https://res.cloudinary.com/dpj8y0x4s/video/upload/v1729063268/20241016-0719-12.9521917_qjswsa.mp4',
-    imageSrc: '/images/project/Linkup.png',
-    title: 'Linkup',
-    tags: ['Nextjs', 'Typescript', 'Convex', 'Clerk', 'TailwindCSS'],
-    timeline: 'July 2024',
-    tagline: 'A simple whatsapp inspired Chat App',
-    sourceCodeHref: 'https://github.com/krey-yon/LinkUp',
-    liveDemoHref: 'https://link-up-three.vercel.app/',
-  },
-  {
-    videoSrc:
-      'https://res.cloudinary.com/dpj8y0x4s/video/upload/v1729063637/20241016-0726-31.5989794_q3bfgg.mp4',
-    imageSrc: '/images/project/StayNest.png',
-    title: 'StayNest',
-    tags: ['Expressjs', 'EJS', 'cloudinary', 'Javascript', 'Bootstrap'],
-    timeline: 'May 2024',
-    tagline: 'Rent your spaces',
-    sourceCodeHref: 'https://github.com/krey-yon/StayNest',
-    liveDemoHref: 'https://staynest-zd60.onrender.com/listings',
+      'https://res.cloudinary.com/deqxdempa/video/upload/v1734584541/gulwtc6wt5mus2bzqj2a.mp4',
+    imageSrc: '/images/project/CTM.png',
+    title: 'CTM',
+    tags: ['React', 'Typescript', 'Shadcn UI', 'Socket.io', 'TailwindCSS'],
+    timeline: 'Dec 2024',
+    tagline: 'A website to collaborate on daily tasks with friends.',
+    sourceCodeHref: 'https://github.com/krey-yon/CTM',
+    liveDemoHref: 'https://ctm.kreyon.tech/',
   },
 ]
